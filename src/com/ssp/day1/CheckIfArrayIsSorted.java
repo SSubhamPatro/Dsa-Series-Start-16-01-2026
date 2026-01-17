c2f436be0cdc5f1949e35e4a17bb04e48719feca
SSubhamPatro <subhampatro160@gmail.com>
2026-01-17 13:10:28 +0530

    Check If Array Is Sorted Or Not

--- src/com/ssp/day1/CheckIfArrayIsSorted.java
+++ src/com/ssp/day1/CheckIfArrayIsSorted.java
@@ -1,6 +1,27 @@
 package com.ssp.day1;
 
+import java.util.Iterator;
+
 public class CheckIfArrayIsSorted {
+
+	public static String isSorted(int[] arr) {
+		
+		boolean ascendingOrder = true;
+        boolean descendingOrder = true;
+        
+        for(int i =0;i<arr.length-1;i++) {
+        	
+        	if(arr[i]>arr[i+1]) {
+        		ascendingOrder =false;
+        	}
+        	if(arr[i]<arr[i+1]) {
+        		descendingOrder =false;
+        	}
+        }
+        if(ascendingOrder) return "Array Is In Ascending Order";
+        if(descendingOrder) return "Array Is In Descending Order";
+        return "Array Is Not Sorted";
+	}
 
 	public static void main(String[] args) {
 
@@ -13,9 +34,22 @@ public class CheckIfArrayIsSorted {
 		Input: [1, 2, 3, 4, 5]
 		Output: true
 		  */
-		int arr[]= {1, 2, 3, 4, 5};
+		int arr[] = { 5,3,4,2,1 };
+		/*	boolean ascendingOrder = false;
+			for (int i = 0; i < arr.length - 1; i++) {
+				// 0=1 0+1=1=2
+				if (arr[i] > arr[i + 1]) {
+					ascendingOrder = true;
+				}
+			}
 		
+			if (!ascendingOrder) {
+				System.out.println("Array is sorted in ascending order");
+			} else {
+				System.out.println("Array is NOT sorted");
+			}*/
 		
+		System.out.println(isSorted(arr));
 	}
 
 }
